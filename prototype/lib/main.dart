f8bb3b640895824c31440e4a621a9303684af9fb
Prahlad <neowakesup7@gmail.com>
2019-02-09 11:19:47 +0530

    Added  Stateful Widget & List

--- prototype/lib/main.dart
+++ prototype/lib/main.dart
@@ -7,11 +7,10 @@ void main() {
 class MyApp extends StatefulWidget {
   @override
   State<StatefulWidget> createState() {
-    // TODO: implement createState
     return _MyAppState();
   }
 }
-
+ 
 class _MyAppState extends State<MyApp> {
   List<String> _products = ['Food Tester'];
 
@@ -27,7 +26,13 @@ class _MyAppState extends State<MyApp> {
               Container(
                 margin: EdgeInsets.all(10.0),
                 child: RaisedButton(
-                  onPressed: () {},
+                  onPressed: () {
+                    setState(() {
+                      _products.add('Advanced Food Tester');
+                    print(_products);
+                    });
+                    
+                  },
                   child: Text('Add Product'),
                 ),
               ),
